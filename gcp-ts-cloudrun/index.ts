2dea854244bf0617af4c83550cf2f5f808ee6673
yabin <yabin@yabins-Mac-mini.local>
2023-05-23 18:15:23 -0700

    new deployment

---
+++ gcp-ts-cloudrun/index.ts
@@ -0,0 +1,51 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+import * as docker from "@pulumi/docker";
+import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
+
+// Location to deploy Cloud Run services
+const location = gcp.config.region || "us-central1";
+
+// -------------------------------------- //
+// Deploy a custom container to Cloud Run //
+// -------------------------------------- //
+
+// Build a Docker image from our sample Ruby app and put it to Google Container Registry.
+// Note: Run `gcloud auth configure-docker` in your command line to configure auth to GCR.
+const imageName = "angular-app";
+const myImage = new docker.Image(imageName, {
+    imageName: pulumi.interpolate`gcr.io/${gcp.config.project}/${imageName}:v1.0.0`,
+    build: {
+        context: "../",
+    },
+});
+
+// Deploy to Cloud Run. Some extra parameters like concurrency and memory are set for illustration purpose.
+const angularService = new gcp.cloudrun.Service("angular", {
+    location,
+    template: {
+        spec: {
+            containers: [{
+                image: myImage.imageName,
+                resources: {
+                    limits: {
+                        memory: "1Gi",
+                    },
+                },
+            }],
+            containerConcurrency: 50,
+        },
+    },
+});
+
+// Open the service to public unrestricted access
+const iamAngular = new gcp.cloudrun.IamMember("angular-everyone", {
+    service: angularService.name,
+    location,
+    role: "roles/run.invoker",
+    member: "allUsers",
+});
+
+// Export the URL
+export const angularUrl = angularService.statuses[0].url;
